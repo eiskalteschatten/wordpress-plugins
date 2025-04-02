a41677773b9020b64935815c6edba0003a9bcee8
Alex <alex@alexseifert.com>
2025-04-02 17:30:14 +0200

    Use WordPress's scaffolding to create a new plugin with an editor block

---
+++ as-image-carousel/build/as-image-carousel/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'b9d5fb58f083ff906aaa');
---
+++ as-image-carousel/build/blocks-manifest.php
@@ -0,0 +1,25 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'as-image-carousel' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'as-image-carousel/as-image-carousel',
+		'version' => '0.1.0',
+		'title' => 'Image Carousel',
+		'category' => 'media',
+		'icon' => 'images-alt',
+		'description' => 'Create an image carousel',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'as-image-carousel',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'viewScript' => 'file:./view.js'
+	)
+);
